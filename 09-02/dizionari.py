a77a821762f176f7c79d947a9fee695d2cbf6b71
GabTech <gabriele1640@gmail.com>
2026-02-09 17:57:53 +0100

    Add files via upload

---
+++ 09-02/dizionari.py
@@ -0,0 +1,21 @@
+studente = {
+    "nome" : "alice",
+    "eta" : 21,
+    "sesso" : "femmina"
+    
+    
+
+    
+    
+    
+}
+
+
+
+
+print (studente["nome"])
+print (studente["eta"])
+
+
+print(studente.keys())
+print(studente.values())
