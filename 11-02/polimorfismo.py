0ea6793fb23c189255d3c8b68a641406a4d395e9
cosograndioso <gabriele1640@gmail.com>
2026-02-11 17:57:20 +0100

    Create polimorfismo.py

---
+++ 11-02/polimorfismo.py
@@ -0,0 +1,17 @@
+class animale:
+    def emetti(self):
+        print("animale fa suono")
+        
+        
+class cane(animale):
+    def emetti(self):
+        print("bau")
+
+
+
+class gatto(animale):
+    def emetti (self):
+        print("miao")
+        
+        
+        
