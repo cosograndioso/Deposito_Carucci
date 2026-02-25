6f9ce1720265c0b6f294f77ffcd619f0b76c5833
GabTech <gabriele1640@gmail.com>
2026-02-25 12:00:14 +0100

    c

--- 25-02/panda.py
+++ 25-02/panda.py
@@ -1,7 +1,27 @@
 import pandas as pd
 
-file_path = 'vendite.csv'
+# Creazione di un DataFrame con dati di esempio
+data = {
+'Nome': ['Alice', 'Bob', 'Carla'],
+'Età': [25, 30, 22],
+'Città': ['Roma', 'Milano', 'Napoli']
+}
+df = pd.DataFrame(data)
 
-df = pd.read_csv(file_path)
+# Stampa del DataFrame originale
+print("DataFrame Originale:")
+print(df)
 
-print(df.head())
+#Selezione delle righe dove l'età è superiore a 23
+df_older = df[df['Età'] > 23]
+
+# Stampa delle righe selezionate
+print("\nPersone con età superiore a 23 anni:")
+print(df_older)
+
+# Aggiungiamo una nuova colonna la persona maggiorenne
+df['Maggiorenne'] = df['Età'] >= 18
+
+# Stampa del DataFrame con la nuova colonna
+print("\nDataFrame con colonna 'Maggiorenne':")
+print(df)
